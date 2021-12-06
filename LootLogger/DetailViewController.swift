6eca4883a3b73e8336c8b3b4dfe4c09b95ef3033
victorbeltran <saitama5719550@gmail.com>
2021-12-06 16:52:16 -0500

    stack views

---
+++ LootLogger/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  LootLogger
+//
+//  Created by Victor Beltran on 12/6/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var SerialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
+    
+    
+    
+    
+
